85d2fcf3a03001272631523bc2d050c5be5c5b6a
Richard Warburton <richard.warburton@gmail.com>
2010-07-09 21:20:41 +0100

    renamed everything and added pm, action and nick change support

--- src/jizzsea/ui/completers/Completer.java
+++
@@ -1,7 +0,0 @@
-package jizzsea.ui.completers;
-
-public interface Completer {
-
-	public String complete(String initial);
-	
-}
--- src/jizzsea/util/Procedure.java
+++
@@ -1,7 +0,0 @@
-package jizzsea.util;
-
-public interface Procedure<T> {
-
-	public boolean f(T a);
-	
-}
